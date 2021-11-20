14ccf2c1e752d5c0942a59264cda99719629a430
vlacerda <vlacerda@gmail.com>
2021-11-20 14:06:27 +0100

    Updated the stack to be installed

--- README.md
+++ README.md
@@ -21,105 +21,33 @@ xcode-select --install
 ./macSetup.sh
 ```
 
-## **General Configurations & Aliases**
-- [x] Git Config
-- [x] Global Git Ignore
-- [x] Mac Screenshot Config
-- [x] Git Aliases
-- [x] Docker Aliases
-- [x] K8s Aliases
-
 ## **Install Browsers**
 - [x] [Google Chrome](https://www.google.com/chrome/)
-- [ ] [Google Chrome Canary](https://www.google.com/chrome/canary/)
 - [x] [Firefox](https://www.mozilla.org/en-US/firefox/new/)
-- [ ] [Firefox Developer Edition](https://www.mozilla.org/en-US/firefox/developer/)
-
-## **Install HashiCorp Tools**
-- [x] [Terraform](https://www.terraform.io)
-- [x] [Vault](https://www.vaultproject.io)
-- [x] [Consul](https://www.consul.io)
-- [x] [Packer](https://www.packer.io)
-- [x] [Nomad](https://www.nomadproject.io)
-- [x] [Vagrant](https://www.vagrantup.com)
 
 ## **Install Virtualization Tools**
-- [x] [VMWare Fusion](https://www.vmware.com/products/fusion.html)
-- [x] [Oracle Virtualbox](https://www.virtualbox.org)
 - [x] [Docker](https://www.docker.com)
-- [x] [Vagrant Manager](http://vagrantmanager.com)
 
-
-## **Install IDEs (Intellij, Visual Studio, Visual Studio Code, Android Studio)**
-- [x] [Intellij](https://www.jetbrains.com/idea/)
-- [x] [Visual Studio](https://visualstudio.microsoft.com)
+## **Install IDEs (Visual Studio, Visual Studio Code)**
 - [x] [Visual Studio Code](https://code.visualstudio.com)
-- [x] [Android Studio](https://developer.android.com/studio)
 
 
 # **Install important Visual Studio Code Extensions**
 If you want to add any new extension besides all the ones listed below then update vscode-extensions.txt before executing script.
-- [x] [GitLens — Git supercharged](https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens)
-- [x] [GitHub](https://marketplace.visualstudio.com/items?itemName=KnisterPeter.vscode-github)
-- [x] [Live Share](https://marketplace.visualstudio.com/items?itemName=MS-vsliveshare.vsliveshare)
-- [x] [Quokka.js](https://marketplace.visualstudio.com/items?itemName=WallabyJs.quokka-vscode)
-- [x] [Bracket Pair Colorizer](https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer)
-- [x] [Visual Studio IntelliCode](https://marketplace.visualstudio.com/items?itemName=VisualStudioExptTeam.vscodeintellicode)
-- [x] [Terraform](https://marketplace.visualstudio.com/items?itemName=mauve.terraform)
 - [x] [Docker](https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-docker)
-- [x] [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
-- [x] [Python](https://marketplace.visualstudio.com/items?itemName=ms-python.python)
-- [x] [Path Intellisense](https://marketplace.visualstudio.com/items?itemName=christian-kohler.path-intellisense)
-- [x] [Remote - Containers](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers)
+- [x] [DotENV](https://marketplace.visualstudio.com/items?itemName=mikestead.dotenv)
+- [x] [Live Share](https://marketplace.visualstudio.com/items?itemName=MS-vsliveshare.vsliveshare)
 - [x] [Markdown All in One](https://marketplace.visualstudio.com/items?itemName=yzhang.markdown-all-in-one)
-- [x] [open-in-browser](https://marketplace.visualstudio.com/items?itemName=techer.open-in-browser)
-- [x] [Code Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker)
-- [x] [Auto Rename Tag](https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-rename-tag)
-- [x] [Auto Close Tag](https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-close-tag)
-- [x] [Polacode](https://marketplace.visualstudio.com/items?itemName=pnp.polacode)
-- [x] [Code Runner](https://marketplace.visualstudio.com/items?itemName=formulahendry.code-runner)
-- [x] [Paste JSON as Code](https://marketplace.visualstudio.com/items?itemName=quicktype.quicktype)
-- [x] [TODO Highlight](https://marketplace.visualstudio.com/items?itemName=wayou.vscode-todo-highlight)
-- [x] [npm](https://marketplace.visualstudio.com/items?itemName=eg2.vscode-npm-script)
-- [x] [Regex Previewer](https://marketplace.visualstudio.com/items?itemName=chrmarti.regex)
-- [x] [Git History](https://marketplace.visualstudio.com/items?itemName=donjayamanne.githistory)
-- [x] [AWS Toolkit for Visual Studio Code](https://marketplace.visualstudio.com/items?itemName=AmazonWebServices.aws-toolkit-vscode)
-- [x] [C#](https://marketplace.visualstudio.com/items?itemName=ms-vscode.csharp)
-
-
-## **Install AWS Tools**
-- [x] [AWS CLI](https://aws.amazon.com/cli/)
-- [x] [SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html)
+- [x] [Visual Studio IntelliCode](https://marketplace.visualstudio.com/items?itemName=VisualStudioExptTeam.vscodeintellicode)
+- [x] [XO](https://marketplace.visualstudio.com/items?itemName=samverschueren.linter-xo)
 
 
 ## **Install Developer Utilities**
-- [x] [SDKMan](https://sdkman.io)
-- [x] [Spectacle](https://www.spectacleapp.com)
-- [x] [Tree]()
-- [x] [Ctop](https://ctop.sh)
 - [x] [jq](https://stedolan.github.io/jq/)
-- [x] [Postman](https://www.getpostman.com)
-- [x] [httpie](https://httpie.org)
 - [x] [NVM](https://github.com/nvm-sh/nvm)
-- [x] [Bash Completion](https://github.com/scop/bash-completion)
-
-## **Install Language SDKs/Platforms**
-- [x] [Dotnet SDK](https://dotnet.microsoft.com/download)
-- [x] [Go](https://golang.org)
-
-
-## **Install Database Tools**
-- [x] [PGAdmin](https://www.pgadmin.org)
-- [x] [Mongo Studio 3T](https://studio3t.com)
-
 
 ## **Install Productivity Tools**
 - [x] [Slack](https://slack.com)
-- [x] [Evernote](https://evernote.com)
-- [x] [The Unarchiver](https://theunarchiver.com)
-- [x] [CheatSheet](https://mediaatelier.com/CheatSheet/)
-- [x] [Adobe Creative Cloud](https://www.adobe.com/creativecloud.html)
-
 
 # **Resources**
 - [MAC OSX BASH PROFILE](https://natelandau.com/my-mac-osx-bash_profile/)
--- macSetup.sh
+++ macSetup.sh
@@ -15,17 +15,16 @@ reset=`tput sgr0`
 
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install)"
     brew install caskroom/cask/brew-cask
-    brew tap homebrew/cask-versions
-    brew tap homebrew/cask-cask
-    brew tap 'homebrew/bundle'
-    brew tap 'homebrew/cask'
-    brew tap 'homebrew/cask-drivers'
-    brew tap 'homebrew/cask-fonts'
-    brew tap 'homebrew/core'
-    brew tap 'homebrew/services'
-    brew tap aws/tap
+    #brew tap homebrew/cask-versions
+    #brew tap homebrew/cask-cask
+    #brew tap 'homebrew/bundle'
+    #brew tap 'homebrew/cask'
+    #brew tap 'homebrew/cask-drivers'
+    #brew tap 'homebrew/cask-fonts'
+    #brew tap 'homebrew/core'
+    #brew tap 'homebrew/services'
 
 fi
 
@@ -40,82 +39,40 @@ beginDeploy() {
 }
 
 ############# General Tools #############
-beginDeploy "############# General Tools #############"
-echo -n "Do you wish to install General Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read General
-
 CaskGeneralToolList=(
     google-chrome
     firefox
     spotify
 )
+
+beginDeploy "############# General Tools #############"
+echo "Do you wish to install General Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+echo "Items to be installed: ${bold}${green}${CaskGeneralToolList[*]}${reset}"
+read General
+
 if [ "$General" != "${General#[Yy]}" ] ;then
     echo Yes
-    brew install --cask --appdir="/Applications" ${CaskGeneralToolList[@]}
+    brew install -f --cask --appdir="/Applications" ${CaskGeneralToolList[@]}
 else
     echo No
 fi
-
-############# Designer #############
-beginDeploy "############# Designer #############"
-echo -n "Do you wish to install Designer Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read Designer
-
-CaskDesignerToolList=(
-    adobe-creative-cloud
-)
-if [ "$Designer" != "${Designer#[Yy]}" ] ;then
-    echo Yes
-    brew install --cask --appdir="/Applications" ${CaskDesignerToolList[@]}
-else
-    echo No
-fi
-
-
-############# Mobile Developer #############
-beginDeploy "############# Mobile Developer #############"
-echo -n "Do you wish to install Mobile Developer Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read MobileDeveloper
-
-CaskMobileDeveloperToolList=(
-    fastlane
-)
-if [ "$MobileDeveloper" != "${MobileDeveloper#[Yy]}" ] ;then
-    echo Yes
-    brew install --cask ${CaskMobileDeveloperToolList[@]}
-else
-    echo No
-fi
-
 
 ############# Developer Utilities #############
-beginDeploy "############# Developer Utilities #############"
-echo -n "Do you wish to install Developer Utilities (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read DeveloperUtilities
-
 DeveloperUtilitiesList=(
-    tree
-    ctop
     jq
-    httpie
-    yarn
-    yarn-completion
-    netcat
-    nmap
     wget
-    go
+    npm
     nvm
-    bash-completion
 )
 CaskDeveloperUtilitiesList=(
     cheatsheet
-    spectacle
-    postman
-    dotnet-sdk
-    wireshark
-    # google-chrome-canary
-    # firefox-developer-edition
 )
+
+beginDeploy "############# Developer Utilities #############"
+echo -n "Do you wish to install Developer Utilities (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+echo "Items to be installed: ${bold}${green}${DeveloperUtilitiesList[*]} ${CaskDeveloperUtilitiesList[*]}${reset}"
+read DeveloperUtilities
+
 if [ "$DeveloperUtilities" != "${DeveloperUtilities#[Yy]}" ] ;then
     
     echo Yes
@@ -129,59 +86,20 @@ if [ "$DeveloperUtilities" != "${DeveloperUtilities#[Yy]}" ] ;then
     export NVM_DIR="$HOME/.nvm"
         [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
         [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion' >> ~/.bash_profile
-
-
-    echo '
-    # BASH-COMPLETION CONFIG
-    [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"' >> ~/.bash_profile
-
-
-    curl -s "https://get.sdkman.io" | bash
-    echo '
-    # THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-    export SDKMAN_DIR="/Users/mpatel/.sdkman"
-    [[ -s "/Users/mpatel/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"' >> ~/.bash_profile
-else
-    echo No
-fi
-
-
-############# Database Tools #############
-beginDeploy "############# Database Tools #############"
-echo -n "Do you wish to install Database Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read Database
-
-
-DatabaseToolList=(
-    kafkacat
-)
-CaskDatabaseToolList=(
-    pgadmin4
-    studio-3t
-    graphiql
-    azure-data-studio
-)
-if [ "$Database" != "${Database#[Yy]}" ] ;then
-    echo Yes
-    brew install ${DatabaseToolList[@]}
-    brew install --cask ${CaskDatabaseToolList[@]}
-
 else
     echo No
 fi
 
 
 ############# IDEs #############
-beginDeploy "############# IDEs #############"
-echo -n "Do you wish to install IDEs (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read IDEs
-
 CaskIDEsList=(
     visual-studio-code
-    intellij-idea
-    visual-studio
-    android-studio
 )
+beginDeploy "############# IDEs #############"
+echo -n "Do you wish to install IDEs (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+echo "Items to be installed: ${bold}${green}${CaskIDEsList[*]}${reset}"
+read IDEs
+
 if [ "$IDEs" != "${IDEs#[Yy]}" ] ;then
     echo Yes
     brew install --cask --appdir="/Applications" ${CaskIDEsList[@]}
@@ -192,138 +110,47 @@ fi
 
 
 ############# DevOps #############
+CaskDevOpsToolList=(
+    docker
+)
 beginDeploy "############# DevOps #############"
 echo -n "Do you wish to install DevOps Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+echo "Items to be installed: ${bold}${green}${CaskDevOpsToolList[*]}${reset}"
 read DevOps
 
-
-DevOpsToolList=(
-    terraform
-    vault
-    consul
-    nomad
-    packer
-    terragrunt
-    ansible
-    awscli
-    aws-sam-cli
-    kompose
-)
-CaskDevOpsToolList=(
-    vagrant
-    vmware-fusion
-    virtualbox
-    docker
-    vagrant-manager
-)
 if [ "$DevOps" != "${DevOps#[Yy]}" ] ;then
     echo Yes
-    brew install ${DevOpsToolList[@]}
     brew install --cask ${CaskDevOpsToolList[@]}
 
     ## DOCKER APP
-    wget -P ~/Downloads/ https://github.com/docker/app/releases/download/v0.6.0/docker-app-darwin.tar.gz
+    wget -P ~/Downloads/ https://github.com/docker/app/releases/download/v0.8.0/docker-app-darwin.tar.gz
     tar -xvf ~/Downloads/docker-app-darwin.tar.gz -C ~/Downloads/
     mv ~/Downloads/docker-app-darwin /usr/local/bin/docker-app
     rm ~/Downloads/docker-app-darwin.tar.gz
-
-
-    ## Install AWS CLI
-    #pip3 --version
-    #curl -O https://bootstrap.pypa.io/get-pip.py
-    #python3 get-pip.py --user
-    #pip3 install awscli --upgrade --user
-    aws --version
-    #rm get-pip.py
-
-
-    curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-macos.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install
-    aws2 --version
 else
     echo No
 fi
 
 
 ############# Productivity Tools #############
-beginDeploy "############# Productivity Tools #############"
-echo -n "Do you wish to install Productivity Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read Productivity
-
 CaskProductivityToolList=(
     slack
-    evernote
-    the-unarchiver
-    dash
-    gpg-suite
-    microsoft-teams
-    microsoft-office
-    zoomus
+    zoom
 )
+beginDeploy "############# Productivity Tools #############"
+echo -n "Do you wish to install Productivity Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+echo "Items to be installed: ${bold}${green}${CaskProductivityToolList[*]}${reset}"
+read Productivity
+
 if [ "$Productivity" != "${Productivity#[Yy]}" ] ;then
     echo Yes
-    brew install --cask --appdir="/Applications" ${CaskProductivityToolList[@]}
-else
-    echo No
-fi
-
-
-############# Mac Application #############
-beginDeploy "############# Mac Application #############"
-echo -n "Do you wish to install Mac Application (${bold}${green}y${reset}/${bold}${red}n${reset})? "
-read MacApplication
-
-MacApplicationToolList=(
-    409183694 # Keynote
-    409203825 # Numbers
-    409201541 # Pages
-    497799835 # Xcode
-    1450874784 # Transporter
-    1274495053 # Microsoft To Do
-    1295203466 # Microsoft Remote Desktop 10
-    985367838 # Microsoft Outlook
-)
-if [ "$MacApplication" != "${MacApplication#[Yy]}" ] ;then
-    brew install mas
-    mas install ${MacApplicationToolList[@]}
-
-    echo "######### Save screenshots to ${HOME}/Pictures/Screenshots"
-    defaults write com.apple.screencapture location -string "${HOME}/Pictures/Screenshots"
-
-    echo "######### Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF"
-    defaults write com.apple.screencapture type -string "png"
-
+    brew install -f --cask --appdir="/Applications" ${CaskProductivityToolList[@]}
 else
     echo No
 fi
 
 beginDeploy "############# CLEANING HOMEBREW #############"
 brew cleanup
-
-beginDeploy "############# GLOBAL GIT CONFIG #############"
-sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.gitignore >> ~/.gitignore'
-git config --global push.default current
-git config --global core.excludesfile ~/.gitignore
-git config --global user.name "<username>"
-git config --global user.email <email>
-git config --global color.branch auto
-git config --global color.diff auto
-git config --global color.interactive auto
-git config --global color.status auto
-
-beginDeploy "############# ALIASES #############"
-beginDeploy "############# GIT ALIASES #############"
-sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.git_aliases >> ~/.git_aliases'
-source ~/.git_aliases
-
-beginDeploy "############# DOCKER ALIASES #############"
-sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.docker_aliases >> ~/.docker_aliases'
-source ~/.docker_aliases
-
-beginDeploy "############# K8s ALIASES #############"
-sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.kubectl_aliases >> ~/.kubectl_aliases'
-source ~/.kubectl_aliases
 
 beginDeploy "############# SETUP BASH PROFILE #############"
 source ~/.bash_profile
